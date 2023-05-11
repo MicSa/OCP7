21b8c3f7dda18bb12d1dbe9785656fc5e69cfe45
MicSa <mickael.salmon@outlook.com>
2023-05-11 13:10:18 +0200

    Optimised functions

--- bruteforce.py
+++ bruteforce.py
@@ -19,26 +19,30 @@ def read_data(file_path):
         shares.append((row[0], int(row[1]), float(row[2].strip('%')) / 100))
     return shares
 #Fonction algorithmique pour répondre au problème
-def brute_force(shares,max_cost):
-    best_profit=0
-    best_combination=[]
-    
-    for i in range (1,len(shares)+1):
-        for combination in itertools.combinations(shares,i):
-            cost=0
-            profit=0
-            for share in combination:
-                cost+=share[1]
-                profit+=share[2]
-            if cost<=max_cost and profit>best_profit:
-                best_profit=profit
-                best_combination=combination
-    return best_combination,best_profit
+def brute_force(shares, max_cost):
+    best_profit = 0
+    best_combination = []
+
+    for i in range(1, len(shares) + 1):
+        for combination in itertools.combinations(shares, i):
+            cost = sum([share[1] for share in combination])
+            profit = sum([share[1] * share[2] for share in combination])
+
+            if cost <= max_cost and profit > best_profit:
+                best_profit = profit
+                best_combination = combination
+
+    return best_combination, best_profit
 
 #Fonction Main
-def main ():
-    shares=read_data('data.csv')
-    best_combination,best_profit=brute_force(shares,500)
-    print("Meilleur combinaison d'actions : ",best_combination)
-    print("Meilleur profit : ",best_profit)
+def main():
+    shares = read_data("data.csv")
+    max_cost = 500
+    best_combination, best_profit = brute_force(shares, max_cost)
+    
+    print(f"Best combination: {best_combination}")
+    print(f"Best profit: {best_profit:.2f} euros")
+
+if __name__ == "__main__":
+    main()
 
