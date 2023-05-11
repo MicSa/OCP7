9285b74013268866f5cc1bd580b8e85d8fa2ff98
MicSa <mickael.salmon@outlook.com>
2023-05-11 14:28:01 +0200

    finished implementation of algorithm

--- optimised.py
+++ optimised.py
@@ -19,4 +19,39 @@ def knapsack(shares, max_cost):
     num_shares = len(shares)
     dp = [[0] * (max_cost + 1) for _ in range(num_shares + 1)]
     #Mise en place de la boucle 
+    for i in range (1,num_shares + 1):
+        share_name, share_cost, share_profit = shares[i - 1]
+        for j  in range (max_cost + 1):
+            if j < share_cost:
+                dp[i][j] = dp[i - 1][j]
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - share_cost] + share_profit)
+        
+        best_profit = dp[-1][-1]
+        best_combination = []
+        
+        i,j = num_shares, max_cost
+        
+        #boucle while pour trouver la meilleure combinaison
+        while i > 0 and j > 0:
+            if dp[i][j] != dp[i - 1][j]:
+                share_name, share_cost, share_profit = shares[i - 1]
+                if dp[i][j] != dp[i - 1][j]:
+                    best_combination.append(share_name,share_cost,share_profit))
+                    j-= share_cost
+                i-=1
+            
+            return best_combination, best_profit
+        
+def main():
+    shares = read_data("Actions1.csv")
+    max_cost = 500
+    best_combination, best_profit = knapsack(shares, max_cost)
     
+    print(f"Best combination: {best_combination}")
+    print(f"Best profit: {best_profit:.2f} euros")
+
+if __name__ == "__main__":
+    main()
+            
+        
