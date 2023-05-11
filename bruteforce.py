d91b3e7b6ce740533c2300121aa653d749b88c33
MicSa <mickael.salmon@outlook.com>
2023-05-11 12:43:31 +0200

    First function

--- bruteforce.py
+++ bruteforce.py
@@ -1,16 +1,24 @@
-﻿//Programem Python
-// Algorithme 
-//Chaque action ne peut être achetée qu'une seule fois.
-//Ne pas acheter une fraction d'action.
-//Dépenser au maximum 500 euros par client.
+﻿#Programem Python
+#Algorithme 
+#Chaque action ne peut être achetée qu'une seule fois.
+#Ne pas acheter une fraction d'action.
+#Dépenser au maximum 500 euros par client.
 
-//Importation des modules 
+#Importation des modules 
 import itertools
 import csv
 
-//Définition des fonctions 
+#Définition des fonctions 
 
-// Fonction pour lire un fichier de données csv
+#Fonction pour lire un fichier de données csv
+def read_data(file_path):
+    with open(file_path, 'r') as file:
+        data = list(csv.reader(file))
+    shares = []
+    for row in data[1:]:
+        shares.append((row[0], int(row[1]), float(row[2].strip('%')) / 100))
+    return shares
+#Fonction algorithmique pour répondre au problème
 
-// Fonction algorithmique pour répondre au problème
+#Fonction Main
 
