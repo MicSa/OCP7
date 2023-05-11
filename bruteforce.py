b79c202b9413ab616858f18afc4378990f42fa6b
MicSa <mickael.salmon@outlook.com>
2023-05-11 12:39:18 +0200

    Base du programme

---
+++ bruteforce.py
@@ -0,0 +1,16 @@
+﻿//Programem Python
+// Algorithme 
+//Chaque action ne peut être achetée qu'une seule fois.
+//Ne pas acheter une fraction d'action.
+//Dépenser au maximum 500 euros par client.
+
+//Importation des modules 
+import itertools
+import csv
+
+//Définition des fonctions 
+
+// Fonction pour lire un fichier de données csv
+
+// Fonction algorithmique pour répondre au problème
+
